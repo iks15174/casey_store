63b9fd1d7494bd6dfd8db0d8717c3b79f15b50a0
jiho <iks15174@daum.net>
2020-09-19 15:18:20 +0900

    second commit

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var router = require('./router/main')(app);
 
 app.set('views', __dirname + '/views');
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 var server = app.listen(3000, function(){
