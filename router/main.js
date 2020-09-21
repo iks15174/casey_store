8a4e77490c8812dafcaa9329d4fe4f278c622b4c
jiho <iks15174@daum.net>
2020-09-21 18:15:10 +0900

    4th change

--- router/main.js
+++ router/main.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-module.exports = function(app, db)
-{
-     app.get('/',function(req,res){
-        res.render('index.html')
-     });
-
-     app.get('/:store',function(req, res){
-       var sql = `SELECT * FROM store WHERE name = ${store}`;
-=======
 module.exports = function(app, db, store_data, store_schema)
 {
      app.get('/',function(req,res){
@@ -22,32 +12,23 @@ module.exports = function(app, db, store_data, store_schema)
      app.get('/detail/:store',function(req, res){
        var store = req.params.store;
        var sql = `SELECT * FROM store WHERE name = "${store}"`;
->>>>>>> 1ae99f4... add store detail file
        db.all(sql,[], (err, rows) => {
          if(err){
            console.log(err.message);
            return;
          }
          else{
-<<<<<<< HEAD
-
-=======
            console.log(rows);
            res.render('store_detail.ejs', {data : rows});
->>>>>>> 1ae99f4... add store detail file
          }
        })
      });
 
-<<<<<<< HEAD
      app.get('/new/:store',function(req,res){
        res.send('hello');
      });
 
-     app.post('/new/:store',function(req,res){
-=======
      app.get('/claim_list/:store',function(req,res){
->>>>>>> 1ae99f4... add store detail file
        res.send('hello');
      })
 
--- server.js
+++ server.js
@@ -2,14 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
-<<<<<<< HEAD
-const table_schema = require('./table_schema');
-const db;
-const router = require('./router/main')(app, db);
-=======
 const store_data = require('./database/store_data');
 const store_schema = require('./database/store_schema');
->>>>>>> 1ae99f4... add store detail file
 
 app.set('views', __dirname + '/views');
 app.use(express.static('public'));
@@ -18,43 +12,7 @@ app.engine('html', require('ejs').renderFile);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-<<<<<<< HEAD
 
-var server = app.listen(3000, function(){
-    console.log("Express server has started on port 3000")
-    db = new sqlite3.Database(':memory:', (err) => {
-      if(err){
-        console.error(err.message);
-        return;
-      }
-      console.log('Sqlite3 database connected to memory')
-    });
-    var db_str = 'CREATE TABLE store (';
-    for(var i = 0; i < table_schema.length; i++){
-      var colum = table_schema.colum[i];
-      var attribute = table_schema.attribute[i];
-      if(i==table_schema.length-1){
-        db_str = db_str + ' ' + colum + ' ' + attribute + ')';
-      }
-      else{
-        db_str = db_str + ' ' + colum + ' ' + attribute + ',';
-      }
-    }
-
-    db.run(db_str, function(err){
-      if(err){
-        console.log(err.message);
-        return;
-      }
-      console.log('Create Store table in memory');
-    });
-
-})
-
-var temp_insert_function = function(){
-
-}
-=======
 const db = new sqlite3.Database(':memory:', (err) => {
   if(err){
     console.error(err.message);
@@ -70,4 +28,3 @@ const router = require('./router/main')(app, db, store_data, store_schema);
 var server = app.listen(3000, function(){
     console.log("Express server has started on port 3000")
 });
->>>>>>> 1ae99f4... add store detail file
