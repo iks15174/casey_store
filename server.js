f905303250653ee56d77e5f11b079af3cd51af76
PARKJIHO <iks15174@daum.net>
2020-10-07 20:07:46 +0900

    heroku version8

--- server.js
+++ server.js
@@ -27,12 +27,6 @@ app.use(session({
   resave : false,
   saveUninitialized : true,
   secret : "12%#$HGJ1231*^%&12" || process.env.SESSION_SECRET, //need to change env variable
-  cookie :{
-    httpOnly : true,
-    secure : true,
-    sameSite : true,
-    maxAge : 600000
-  },
 }));
 app.use(methodOverride('_method'));
 app.use(passport.initialize());
